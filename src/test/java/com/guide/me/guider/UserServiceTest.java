91b666e90d2e242d58039e65f6dfdb28bd8db936
Wassim <fkirwassoum22@gmail.com>
2026-06-21 16:23:07 +0100

    Tests unitaires

---
+++ src/test/java/com/guide/me/guider/UserServiceTest.java
@@ -0,0 +1,15 @@
+package com.guide.me.guider;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void simpleTest() {
+        int a = 2;
+        int b = 3;
+
+        assertEquals(5, a + b);
+    }
+}
